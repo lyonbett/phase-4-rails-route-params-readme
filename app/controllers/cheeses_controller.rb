7cefacde69226a082c5f067d7615d1e99fdfd9a0
lyonbett <lyon.bett@student.moringaschool.com>
2023-07-01 01:58:56 +0300

    completed assignment

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -5,4 +5,11 @@ class CheesesController < ApplicationController
     render json: cheeses
   end
 
+  def show
+    #find a cheese using ID from the url
+    #send a json response using that cheese object
+    cheese = Cheese.find(params[:id])
+    render json: cheese
+  end
+
 end
